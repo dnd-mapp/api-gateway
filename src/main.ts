b735a3846b5619dc29d44c9f25f836ae83fb7e23
NoNamer777 <oscar.wellner@gmail.com>
2026-02-24 15:11:35 +0100

    Feat: Log server address when it is running

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule, configureSSL, parseInteger } from './app';
 
@@ -10,7 +11,10 @@ async function main() {
 
     app.enableShutdownHooks();
 
-    await app.listen(port, host);
+    await app.listen(port, host, () => {
+        const address = `${ssl ? 'https' : 'http'}://${host}:${port}`;
+        Logger.log(`Application is running on: ${address}`, 'NestApplication');
+    });
 }
 
 main().catch((error) => console.error(error));
