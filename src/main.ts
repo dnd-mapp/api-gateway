58c7e2b59f386225ec0fc2fd2f139f49c7ccd8da
NoNamer777 <oscar.wellner@gmail.com>
2026-02-24 12:20:26 +0100

    Refactor: Move and rename parseInteger function to utils

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter } from '@nestjs/platform-fastify';
-import { AppModule } from './app/app.module';
-
-function parseNumber(fallback: number, value?: string) {
-    if (!value) return fallback;
-    const parsed = Number.parseInt(value);
-
-    if (Number.isNaN(parsed)) return fallback;
-    return parsed;
-}
+import { AppModule, parseInteger } from './app';
 
 async function main() {
     const adapter = new FastifyAdapter();
+
     const app = await NestFactory.create(AppModule, adapter);
 
     const host = process.env['HOST'] ?? 'localhost';
-    const port = parseNumber(3000, process.env['PORT']);
+    const port = parseInteger(3000, process.env['PORT']);
 
     app.enableShutdownHooks();
 
