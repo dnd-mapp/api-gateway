e3f4af39ae90f3186cbccc2e8280c521506d272b
Oscar Wellner <oscar.wellner@gmail.com>
2026-02-25 17:24:39 +0100

    Feat: Add Spell model

---
+++ src/app/spell/spell.builder.ts
@@ -0,0 +1,19 @@
+import { SpellDto } from './spell.dto';
+
+export class SpellBuilder {
+    private readonly spell = new SpellDto();
+
+    public build() {
+        return this.spell;
+    }
+
+    public withId(id: string) {
+        this.spell.id = id;
+        return this;
+    }
+
+    public withName(name: string) {
+        this.spell.name = name;
+        return this;
+    }
+}
