e648da0531cb878d0a44594e3e1ce7daec2a6e80
Oscar Wellner <oscar.wellner@gmail.com>
2026-02-25 17:04:32 +0100

    Feat: Check database connection as part of the readiness check

--- src/app/health/health.controller.ts
+++ src/app/health/health.controller.ts
@@ -1,21 +1,34 @@
 import { Controller, Get } from '@nestjs/common';
-import { HealthCheckService } from '@nestjs/terminus';
+import { HealthCheck, HealthCheckService, PrismaHealthIndicator } from '@nestjs/terminus';
+import { DatabaseService } from '../core';
 
 @Controller('/health')
 export class HealthController {
-    private healthCheckService: HealthCheckService;
+    private readonly healthCheckService: HealthCheckService;
+    private readonly prismaHealthIndicator: PrismaHealthIndicator;
+    private readonly databaseService: DatabaseService;
 
-    constructor(healthCheckService: HealthCheckService) {
+    constructor(
+        healthCheckService: HealthCheckService,
+        prismaHealthIndicator: PrismaHealthIndicator,
+        databaseService: DatabaseService,
+    ) {
         this.healthCheckService = healthCheckService;
+        this.prismaHealthIndicator = prismaHealthIndicator;
+        this.databaseService = databaseService;
     }
 
+    @HealthCheck()
     @Get('/live')
     public async livenessCheck() {
         return await this.healthCheckService.check([]);
     }
 
+    @HealthCheck()
     @Get('/ready')
     public async ready() {
-        return await this.healthCheckService.check([]);
+        return await this.healthCheckService.check([
+            async () => await this.prismaHealthIndicator.pingCheck('database', this.databaseService.prisma),
+        ]);
     }
 }
--- src/app/health/health.module.ts
+++ src/app/health/health.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TerminusModule } from '@nestjs/terminus';
+import { DatabaseModule } from '../core';
 import { HealthController } from './health.controller';
 
 @Module({
-    imports: [TerminusModule],
+    imports: [TerminusModule, DatabaseModule],
     controllers: [HealthController],
 })
 export class HealthModule {}
